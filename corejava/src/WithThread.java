08d647cf9864181fe9da2b3127de03009f390ed2
Jatdevta <138859530+Jatdevta@users.noreply.github.com>
2023-07-09 00:41:28 +0530

    Add files via upload

---
+++ corejava/src/WithThread.java
@@ -0,0 +1,15 @@
+package thread;
+
+public class WithThread extends Thread{
+	public String name = null;
+	public WithThread(String name) {
+		this.name=name;
+	}
+@Override
+public void run() {
+	for (int i = 1; i <=5; i++) {
+		System.out.println(i+ " " +name);
+		
+	}
+}
+}
